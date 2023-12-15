85839d1794d06d273b9d222dbe87b2c4c38eb8e8
whorishi <0710rishi@gmail.com>
2023-12-16 02:17:11 +0530

    some indentation commit

--- main_test.go
+++ main_test.go
@@ -5,41 +5,42 @@ import (
 	"net/http"
 	"testing"
 	"time"
+
 	"gofr.dev/pkg/gofr/request"
 )
 
 func TestIntegration(t *testing.T) {
 	go main()
-	time.Sleep(3* time.Second)
+	time.Sleep(3 * time.Second)
 
 	tests := []struct {
-		desc string
+		desc       string
 		method     string
 		endpoint   string
 		statusCode int
 		body       []byte
 	}{
-		{"get books",http.MethodGet,"books",http.StatusOK,nil},
-		{"post books",http.MethodPost,"books",http.StatusCreated,[]byte(`{
+		{"get books", http.MethodGet, "books", http.StatusOK, nil},
+		{"post books", http.MethodPost, "books", http.StatusCreated, []byte(`{
 				"title": "hiiiiii",
 				"author": "hellllo",
 				"publisher": "holla",
 				"price": 123,
 				"category": "sayonara"
 		}`),
-	}}
+		}}
 
-	for i,tc := range tests {
-		req,_ := request.NewMock(tc.method,"http://localhost:8000/"+tc.endpoint,bytes.NewBuffer(tc.body))
-		c:=http.Client{}
+	for i, tc := range tests {
+		req, _ := request.NewMock(tc.method, "http://localhost:8000/"+tc.endpoint, bytes.NewBuffer(tc.body))
+		c := http.Client{}
 
 		resp, err := c.Do(req)
-		if err!= nil {
+		if err != nil {
 			t.Errorf("TEST[%v] Failed.\tHTTP request encountered Err: %v\n%s", i, err, tc.desc)
 			continue
 		}
-		
-		if resp.StatusCode!=tc.statusCode {
+
+		if resp.StatusCode != tc.statusCode {
 			t.Errorf("TEST[%v] Failed.\tExpected %v\tGot %v\n%s", i, tc.statusCode, resp.StatusCode, tc.desc)
 		}
 
@@ -47,5 +48,3 @@ func TestIntegration(t *testing.T) {
 	}
 
 }
-
-
